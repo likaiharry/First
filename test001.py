2a7b2b8c9e998a14264028b803dee4efbe5566c8
likaiharry <likaiharry@163.com>
2017-03-20 15:05:59 +0800

    Add 2

--- test001.py
+++ test001.py
@@ -1,6 +1,5 @@
 # Fileneme: test001.py
 
-
 import string
 
 def reverse(text):
@@ -8,3 +7,22 @@ def reverse(text):
 
 def is_palindrome(text):
     text = text.lower()
+    print(text)
+    text = text.replace(' ', '')
+    print(text)
+    for char in string.punctuation:
+        text = text.replace(char, '')
+    print(text)
+    return text == reverse(text)
+
+def main():
+    something = input('Enter text:')
+    if (is_palindrome(something)):
+        print("Yes, it is")
+    else:
+        print("No, it is not")
+
+if __name__ == '__main__':
+    main()
+else:
+    print("it was improted!")
