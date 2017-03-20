519ffa942eedfe048e72e8fa8babbc875c0bcc97
likaiharry <likaiharry@163.com>
2017-03-20 15:06:20 +0800

    Revert "Add 2"
    This reverts commit 2a7b2b8c9e998a14264028b803dee4efbe5566c8.

--- test001.py
+++ test001.py
@@ -1,5 +1,6 @@
 # Fileneme: test001.py
 
+
 import string
 
 def reverse(text):
@@ -7,22 +8,3 @@ def reverse(text):
 
 def is_palindrome(text):
     text = text.lower()
-    print(text)
-    text = text.replace(' ', '')
-    print(text)
-    for char in string.punctuation:
-        text = text.replace(char, '')
-    print(text)
-    return text == reverse(text)
-
-def main():
-    something = input('Enter text:')
-    if (is_palindrome(something)):
-        print("Yes, it is")
-    else:
-        print("No, it is not")
-
-if __name__ == '__main__':
-    main()
-else:
-    print("it was improted!")
